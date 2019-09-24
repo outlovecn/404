71599f1c31c39c036b9b0e9c5ff8b438987ed0b3
Chaos <aniyocc@gmail.com>
2019-09-25 00:33:23 +0800

    完善打包脚本， 移除dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-const { src, dest, parallel } = require('gulp')
+const { src, dest, parallel, series } = require('gulp')
 const uglify = require('gulp-uglify')
-// const rename = require('gulp-rename')
+const rename = require('gulp-rename')
 const htmlmin = require('gulp-html-minifier2')
 const cssmin = require('gulp-cssmin')
+const inlinesource = require('gulp-inline-source')
+const obfuscate = require('gulp-obfuscate')
+const del = require('del')
 function js () {
   return src('js/*.js')
     .pipe(uglify())
-    // .pipe(rename({ extname: '.min.js' }))
+    // .pipe(obfuscate())
     .pipe(dest('dist/js'))
 }
 function html () {
-  return src('./*.html')
+  return src('index.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(dest('dist'))
 }
@@ -19,9 +22,20 @@ function css () {
     .pipe(cssmin())
     .pipe(dest('dist/css'))
 }
+function clean (cb) {
+  del.sync(['dist/**', '!dist/404.html'])
+  cb()
+}
+function inline () {
+  return src('dist/index.html')
+    .pipe(inlinesource())
+    .pipe(rename("404.html"))
+    .pipe(dest('dist'))
+}
 exports = Object.assign(exports, {
   js,
   html,
-  css
+  css,
+  clean
 })
-exports.default = parallel(html, js, css)
+exports.default = series(parallel(html, js, css), inline, clean)
